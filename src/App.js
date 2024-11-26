c8f817d7d860f9bccb9dcc8ff5093a6d639fef49
ygvbhy <ygvbhy@naver.com>
2024-11-26 22:57:06 +0900

    +,- Button Add

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  // data-testid 로 해서 테스트 할 수 있도록 함
+  // 해당 아이디 값으로 app.test.js 에서 불러와서 테스트 가능
   return (
     <div className="App">
       <div className="App-Header">
         <h3 data-testid="counter">{counter}</h3>
+        <div>
+          <button data-testid="plus-button">+</button>
+          <button data-testid="minus-button">-</button>
+        </div>
       </div>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// 카운터가 0 인지 테스트
 test("the counterr starts at 0", () => {
   render(<App />);
   const counterElement = screen.getByTestId("counter");
   expect(counterElement).toHaveTextContent(0);
+});
+
+// 마이너스 버튼의 텍스트가 '-' 인지 테스트
+test("minus button has correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+});
+
+// 플러스 버튼의 텍스트가 '+' 인지 테스트
+test("plus button has correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
 });
