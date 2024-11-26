fd15a9ad1a05009e5f28f2e45fb411ed679bfaff
ygvbhy <ygvbhy@naver.com>
2024-11-26 23:02:52 +0900

    +, - 버튼 기능 넣기 및 테스트

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+
   // data-testid 로 해서 테스트 할 수 있도록 함
   // 해당 아이디 값으로 app.test.js 에서 불러와서 테스트 가능
   return (
@@ -10,8 +11,18 @@ function App() {
       <div className="App-Header">
         <h3 data-testid="counter">{counter}</h3>
         <div>
-          <button data-testid="plus-button">+</button>
-          <button data-testid="minus-button">-</button>
+          <button
+            data-testid="minus-button"
+            onClick={() => setCounter(counter - 1)}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            onClick={() => setCounter(counter + 1)}
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 // 카운터가 0 인지 테스트
@@ -20,4 +20,22 @@ test("plus button has correct text", () => {
   render(<App />);
   const buttonElement = screen.getByTestId("plus-button");
   expect(buttonElement).toHaveTextContent("+");
+});
+
+// 플러스 버튼을 누르면 카운터가 1 증가하는지 테스트
+test("when the plus button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+// 마이너스 버튼을 누르면 카운터가 1 감소하는지 테스트
+test("when the minus button is pressed, the counter changes to -1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
 });
